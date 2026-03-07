363da0abc6b76c05e4e261a6d864ccb1c1941224
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-07 13:40:50 +0300

    start lesson 01 tests

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,14 @@
+const sentense = "Hello my friends";
+
+
+export function sum(a: number, b: number) {
+    return a + b
+}
+
+export function mult(a: number, b: number) {
+    return a * b
+}
+
+function splitIntoWords(sentense: string) {
+    return ["Hello", "my", "friends"]
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import {StrictMode} from 'react'
+// import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {value01_01} from "./01/01.ts";
+// import {value01_01} from "./01/01.ts";
 
-let a = value01_01
+// let a = value01_01
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
+    // <StrictMode>
         <App/>
-    </StrictMode>,
+    // </StrictMode>,
 )
