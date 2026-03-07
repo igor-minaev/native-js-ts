e8456dfa0d07a35145c056cf52caacd611847602
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-07 12:45:10 +0300

    completed lesson 00

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {value01_01} from "./01/01.ts";
+
+let a = value01_01
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App/>
+    </StrictMode>,
 )
