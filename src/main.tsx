0d8be2aaf408fd19d6a9b0379877a25adc423059
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-22 12:31:14 +0300

    start lesson 06 callback, onClick, onChange, onBlur

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { User } from "./05/06-callbacks.tsx"
 // import {value01_01} from "./01/01.ts";
 
 // let a = value01_01
 
 createRoot(document.getElementById('root')!).render(
     // <StrictMode>
-        <App/>
+        <User/>
     // </StrictMode>,
 )
