731b0ee24bf6714ce26e7b8f55979a2ec2d9c395
Igor Minayeu <igor.minayeu@gmail.com>
2026-04-13 19:54:31 +0300

    start lesson 8 associative array

---
+++ src/08-associative_array/08_01.test.ts
@@ -0,0 +1,10 @@
+export const users = {
+    '101': {id: 101, name: 'Dimych'},
+    '3232312': {id: 3232312, name: 'Natasha'},
+    '1212': {id: 1212, name: 'Valera'},
+    '1': {id: 1, name: 'Katya'}
+}
+
+export const usersArray = [
+    {id: 101, name: 'Dimych'}, {id: 3232312, name: 'Natasha'}, {id: 1212, name: 'Valera'}, {id: 1, name: 'Katya'}
+]
